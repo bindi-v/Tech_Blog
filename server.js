97d5ffd5b7d74d650a48e8d31e04bb3ef7a8f104
bindi-v <bindi.vaghela@gmail.com>
2021-10-25 21:24:08 -0400

    create models, controller folder

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const sequelize = require('./config/config');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//const hbs = exphbs.create({ helpers });
+
+//app.engine('handlebars', hbs.engine);
+//app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
