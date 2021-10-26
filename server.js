6ca4df5c6864d154bcf95cfb464e02bb1eb612d4
bindi-v <bindi.vaghela@gmail.com>
2021-10-26 19:11:26 -0400

    added login template in login.handlebars

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
-//app.engine('handlebars', hbs.engine);
-//app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
