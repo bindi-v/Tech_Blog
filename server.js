9221c212665bf10db6c507ef5c768cad3ab5c325
bindi-v <bindi.vaghela@gmail.com>
2021-10-26 20:48:13 -0400

    created models and modified server.js and controller

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
 const express = require('express');
-const sequelize = require('./config/config');
+
 const exphbs = require('express-handlebars');
+const path = require('path');
+const sequelize = require('./config/config');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ format_date: date => {
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  } });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+const sess = {
+  secret: 'Super secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(require('./controller/'));
+
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
+    sequelize.sync({ force: false });
 });
