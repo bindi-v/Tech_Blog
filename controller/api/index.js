18ad01a0b0ac76bc427e732651876f6df533cdee
bindi-v <bindi.vaghela@gmail.com>
2021-10-26 21:30:35 -0400

    fixed bugs to run server

--- controller/api/index.js
+++ controller/api/index.js
@@ -1,70 +1,8 @@
 const router = require('express').Router();
+const userRoutes = require('./user-routes.js');
 
-const apiRoutes = require('./api');
 
-const { User } = require('../../models');
-const { Post } = require('../../models/');
-router.post('/', async (req, res) => {
-    try {
-      const newUser = await User.create({
-        username: req.body.username,
-        password: req.body.password,
-      });
-    
-      req.session.save(() => {
-        req.session.userId = newUser.id;
-        req.session.username = newUser.username;
-        req.session.loggedIn = true;
+router.use('/user', userRoutes);
 
-        res.json(newUser);
-    });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-
-  router.post('/login', async (req, res) => {
-    try {
-      const user = await User.findOne({
-        where: {
-          username: req.body.username,
-        },
-      });
-  
-      if (!user) {
-        res.status(400).json({ message: 'No user account found!' });
-        return;
-      }
-  
-      const validPassword = user.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res.status(400).json({ message: 'No user account found!' });
-        return;
-      }
-  
-      req.session.save(() => {
-        req.session.userId = user.id;
-        req.session.username = user.username;
-        req.session.loggedIn = true;
-  
-        res.json({ user, message: 'You are now logged in!' });
-      });
-    } catch (err) {
-      res.status(400).json({ message: 'No user account found!' });
-    }
-  });
-  
-  router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    } else {
-      res.status(404).end();
-    }
-  });
-
-router.use('/api', apiRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,24 +1,11 @@
 const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+//const Post = require('./Post');
 
-Post.belongsTo(User, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
-});
 
-Post.hasMany(Comment, {
-  foreignKey: 'postId',
-  onDelete: 'CASCADE'
-});
+//Post.belongsTo(User, {
+//  foreignKey: 'userId',
+//  onDelete: 'CASCADE'
+//});
 
-Comment.belongsTo(User, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
-});
 
-module.exports = {
-  User,
-  Comment,
-  Post
-};
+module.exports = { User };
