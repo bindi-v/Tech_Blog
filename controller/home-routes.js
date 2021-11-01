49eb926195aa45b56906e57eaffb2ce873022419
bindi-v <bindi.vaghela@gmail.com>
2021-11-01 19:45:54 -0400

    signup are taking values

---
+++ controller/home-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {User} = require("../models/");
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+  router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('signup');
+  });
+  
+  module.exports = router;
--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
-
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes')
+//const dboardRoutes = require('./dashboard-routes');
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+//router.use('/dashboard', dboardRoutes);
+
 module.exports = router;
