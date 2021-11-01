10e35e46da27d1ff4faf763642d1f67a374becd3
bindi-v <bindi.vaghela@gmail.com>
2021-11-01 19:58:53 -0400

    added utils and post files

--- controller/home-routes.js
+++ controller/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {User} = require("../models/");
+const {User, Post} = require("../models/");
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
@@ -17,6 +17,43 @@ router.get('/login', (req, res) => {
     }
   
     res.render('signup');
+  });
+  
+  router.get('/', async (req, res) => {
+    try {
+      const postData = await Post.findAll({
+        include: [User],
+      });
+  
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      res.render('all-posts', { posts });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  router.get('/post/:id', async (req, res) => {
+    try {
+      const postData = await Post.findByPk(req.params.id, {
+        include: [
+          User,
+          {
+            model: Comment,
+            include: [User],
+          },
+        ],
+      });
+  
+      if (postData) {
+        const post = postData.get({ plain: true });
+  
+        res.render('single-post', { post });
+      } else {
+        res.status(404).end();
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
   });
   
   module.exports = router;
